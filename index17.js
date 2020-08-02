d0a58c44150dc05b91861466364f23b23c46d827
SLeonSNarro <ssleon.narro@gmail.com>
2020-08-02 13:46:13 +0200

    edit and delete buttons

--- index17.js
+++ index17.js
@@ -2,18 +2,34 @@ function addContact(event) {
 
     var name = document.getElementById("name").value;
     var phone = document.getElementById("phone").value;
-    if (!name || !phone){
+    if (!name || !phone) {
         alert("algo esta vacio")
         return;
     }
 
-    var algo = `<div class="jumbotron">
-    <p class="lead">'${name} - ${phone}</p>
+    var algo = `<div class="jumbotron"><p class="lead">${name} - ${phone}</p>
     <hr class="my-4">
     <p class="lead">
-        <a class="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=qe5-ywmuKOg" role="button">Edit</a>
-        <a class="btn btn-danger btn-lg" href="https://www.youtube.com/watch?v=qe5-ywmuKOg" role="button">Delete</a>
+        <a class="btn btn-primary btn-lg" role="button" onclick="showEdit(event)">Edit</a>
+        <a class="btn btn-danger btn-lg" role="button" onclick="deleteContact(event)">Delete</a>
     </p>
+    <form onsubmit="return editContact(event);" style="display: none;">
+                    <fieldset>
+                        <legend>Editar contacto</legend>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Name</label>
+                            <input type="text" class="form-control" name="name2"
+                                aria-describedby="emailHelp" placeholder="Enter name">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Phone</label>
+                            <input type="number" class="form-control" name="phone"
+                                aria-describedby="emailHelp" placeholder="Enter phone">
+                        </div>
+                    </fieldset>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    </fieldset>
+                </form>
 </div>`
 
     var lista = document.getElementById("Lista");
@@ -21,4 +37,41 @@ function addContact(event) {
 
     return false;
 
+}
+
+function deleteContact(event) {
+    var btn = event.srcElement;
+
+    var jumbo = btn.parentNode.parentNode;
+
+    var divLista = jumbo.parentNode;
+
+    divLista.removeChild(jumbo);
+}
+
+function editContact(event) {
+    var form = event.srcElement;
+
+    var name = form.name2.value;
+    var phone = form.phone.value;
+    if (!name || !phone) {
+        alert("algo esta vacio")
+        return;
+    }
+
+    var lead1 = form.parentNode.firstChild;
+    lead1.innerHTML = name + " - " + phone;
+
+    return false;
+
+}
+
+function showEdit(event) {
+    var btnEdit = event.srcElement;
+    var form = btnEdit.parentElement.nextElementSibling;
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
 }
